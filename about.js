e7e3c4c1011cf5e8618703801e60f4ceba35ad82
tems2548 <tems2548@gmail.com>
2023-06-27 01:03:57 +0700

    ..

---
+++ about.js
@@ -0,0 +1,13 @@
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents =document.getElementsByClassName('tab-contents');
+var tablink;
+var tabcontent;
+function opentab(tabname){
+  for(tablink of tablinks ){
+    tablink.classList.remove("active-link");
+  }
+  for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+  }
+}
+
