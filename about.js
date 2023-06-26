d549938ed634abd1f8e9f30c1f12f9e59c7615d2
tems2548 <tems2548@gmail.com>
2023-06-27 01:16:05 +0700

    about commit

--- about.js
+++ about.js
@@ -9,5 +9,8 @@ function opentab(tabname){
   for(tabcontent of tabcontents){
     tabcontent.classList.remove("active-tab");
   }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
+
 
