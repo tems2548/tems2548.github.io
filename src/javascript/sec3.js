126638641e187bbdecca63198d440dbd56023eae
tems2548 <tems2548@gmail.com>
2023-06-29 13:22:56 +0700

    ..

--- src/javascript/sec3.js
+++
@@ -1,6 +0,0 @@
-$('.button').click(function () {
-    $(this).parent().toggleClass('expanded');
-    $(this).parent().toggleClass('collapsed');
-    $(this).parents('.main-content').toggleClass('active');
-  });
-  
