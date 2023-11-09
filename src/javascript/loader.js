e47c6bb8b8f483c81a9f356824178c8e50a1a9ec
tems2548 <tems2548@gmail.com>
2023-11-09 11:03:06 +0700

    loading when ready

--- src/javascript/loader.js
+++ src/javascript/loader.js
@@ -1,10 +1,17 @@
-var myVar;
+// var myVar;
 
-function loaded() {
-  myVar = setTimeout(showPage, 2000);
-}
+// function loaded() {
+//   myVar = setTimeout(showPage, 2000);
+//   document.onreadystatechange ()=>{}
+// }
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("my-con").style.display = "block";
+}
+
+document.onreadystatechange = ()=>{
+  if(document.readyState === "complete"){
+    showPage();
+  }
 }
