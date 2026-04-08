d3befd76492bcf481ac0f08a1bc46d105658e42d
tems2548 <tems2548@gmail.com>
2026-04-08 21:10:25 +0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/new_portfolio/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
